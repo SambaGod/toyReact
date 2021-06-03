bba4640a331bde2411872faf8dd0037d86346281
Waleed <waleed.gatosorriso@gmail.com>
2021-06-03 15:50:55 +0200

    Added comments and layout fix

--- toy-react/src/components/DishdetailComponent.js
+++ toy-react/src/components/DishdetailComponent.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Card, CardImg, CardTitle, CardText, CardBody } from 'reactstrap';
 
 class DishDetail extends Component {
+  // renderDish simply takes dish as an argument and fills in the card below
   renderDish(dish) {
     if (dish != null) {
       return (
@@ -19,17 +20,40 @@ class DishDetail extends Component {
   }
 
   render() {
+    // Instead of using a renderComments function which I find unnecessary, I simply iterated over the received information via comments props
     const kommente =
-      this.props.comments != null
+      this.props.comments != null // Run the code below if comments are not empty
         ? this.props.comments.map(komment => {
-            return <div key={komment.id}>{komment.comment}</div>;
+            return (
+              <div key={komment.id} className='mb-5'>
+                <p>{komment.comment}</p>
+                <p>
+                  -- {komment.author}{' '}
+                  <span className='text-secondary'>
+                    | {new Date(komment.date).toLocaleDateString('de-de')}{' '}
+                    {/* Formating date to German format */}
+                  </span>
+                </p>
+              </div>
+            );
           })
-        : '';
+        : ''; // Return empty if not comments were passed
 
     return (
-      <div className='row'>
-        <div className='col-6'>{this.renderDish(this.props.dish)}</div>
-        <div className='col-6'>{kommente}</div>
+      <div className='row my-1'>
+        {/* Dish details */}
+        <div className='col-md-6 col-12'>
+          {this.renderDish(this.props.dish)}
+        </div>
+
+        {/* Comments */}
+        <div className='col-md-6 col-12'>
+          {/* show the heading if comments are not empty */}
+          <h4 className={`mt-2 ${this.props.comments == null ? 'd-none' : ''}`}>
+            Comments
+          </h4>
+          {kommente}
+        </div>
       </div>
     );
   }
--- toy-react/src/components/MenuComponent.js
+++ toy-react/src/components/MenuComponent.js
@@ -6,20 +6,23 @@ class Menu extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      // Set both dish and comment details to null in the start
       selectedDish: null,
       selectedDishComments: null,
     };
   }
 
   onDishSelect(dish, comments) {
+    // Change the status of both dish and comments received as arguments via onClick function
     this.setState({ selectedDish: dish });
     this.setState({ selectedDishComments: comments });
   }
 
   render() {
+    // Iterate over all items inside dishes and use "dish" as an iteration variable
     const menu = this.props.dishes.map(dish => {
       return (
-        <div key={dish.id} className='col-md-6 col-sm-12 my-5'>
+        <div key={dish.id} className='col-md-6 col-sm-12 my-1'>
           <Card onClick={() => this.onDishSelect(dish, dish.comments)}>
             <CardImg top width='100%' src={dish.image} alt={dish.name} />
             <CardImgOverlay>
@@ -31,7 +34,11 @@ class Menu extends Component {
     });
     return (
       <div className='container'>
+        {/* Iterated menu items are filled here */}
         <div className='row'>{menu}</div>
+
+        {/* Iterated dish details and comments are filled here in DishDetail component */}
+        {/* dish and comments are passed as props to the component */}
         <DishDetail
           dish={this.state.selectedDish}
           comments={this.state.selectedDishComments}
